4ce1a3714098d26e9efc1331f07bceba2b35aee6
JimmyPhas <JimmyPhasouk@gmail.com>
2020-03-07 14:22:37 -0500

    added detail activity and added layout elements

---
+++ app/src/main/java/com/example/hw3_to_do_list/Details.kt
@@ -0,0 +1,12 @@
+package com.example.hw3_to_do_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Details : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
